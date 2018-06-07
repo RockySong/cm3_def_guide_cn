24ed1878e8e2b56025ae694c614b9ece2b7d932a
rocky <rock.song@hotmail.com>
2018-06-07 20:22:33 +0800

    add more comments

--- utils/profiling.c
+++ utils/profiling.c
@@ -4,9 +4,10 @@ typedef struct {
     uint32_t    r0, r1, r2, r3, r12, lr, pc, xpsr;
 } ExceptionRegisters_t;
 
+#define ARRAY_SIZE(x)	(sizeof(x) / sizeof((x)[0]))
+#define PROF_CNT	20
 #define PROF_ERR	32
 #define PROF_MASK (~(PROF_ERR - 1))
-#define PROF_CNT	20
 
 uint32_t s_ignrList[] = {
 	0,
@@ -15,14 +16,14 @@ uint32_t s_ignrList[] = {
 
 typedef struct {
 	uint32_t baseAddr;	// (aligned) base address range of PC sample
-	uint32_t hitCnt;    // how many hits (note, a decay mecahnism automatically drop hitCnt)
-	uint32_t hitRatio;	// 10-bit resolution hit ratio, i.e., estimation of CPU usage
-	uint32_t rsvd;
+	uint32_t hitCnt;    // hit count (a decay mecahnism automatically drops it)
+	uint32_t hitRatio;	// 10-bit resolution hit ratio, 
+	uint32_t rsvd;		// reserved for better view in memory window
 } ProfUnit_t;
 
 typedef struct {
-	uint8_t decayNdx;
-	uint32_t profCnt;
+	uint8_t decayNdx;  // which item to decay its hitCnt 
+	uint32_t profCnt;  // totoal hit count of profiling
 	ProfUnit_t items[PROF_CNT];
 }Prof_t;
 Prof_t s_prof;
@@ -32,10 +33,12 @@ void _ProfOnHit(ProfUnit_t *pItem, uint32_t pc)
 	pItem->baseAddr = pc & PROF_MASK;
 	s_prof.profCnt+= 0x02;
 	pItem->hitCnt += 0x02;
-	pItem->hitRatio = (uint32_t)(((uint64_t)(pItem->hitCnt) << 10) / s_prof.profCnt);
+	pItem->hitRatio = 
+		(uint32_t)(((uint64_t)(pItem->hitCnt) << 10) / s_prof.profCnt);
 	// sort items descending
 	ProfUnit_t tmpItem;	
-	for (;pItem != s_prof.items && pItem[0].hitCnt > pItem[-1].hitCnt; pItem--) {
+	for (;pItem != s_prof.items && pItem[0].hitCnt > pItem[-1].hitCnt; pItem--) 
+	{
 		tmpItem = pItem[0]; pItem[0] = pItem[-1] ; pItem[-1] = tmpItem;
 	}
 }
@@ -44,10 +47,12 @@ void Profiling(uint32_t pc)
 {
 	uint32_t i;
 	ProfUnit_t *pItem = &s_prof.items[0];
+	// filter ignore list
 	for (i=0; i<ARRAY_SIZE(s_ignrList); i++) {
 		if (pc - s_ignrList[i] < PROF_ERR)
 			return;
 	}
+	// apply decaying
 	if (s_prof.items[s_prof.decayNdx].hitCnt != 0) {
 		s_prof.items[s_prof.decayNdx].hitCnt--;
 		s_prof.profCnt--;
@@ -55,11 +60,13 @@ void Profiling(uint32_t pc)
 	if (++s_prof.decayNdx == PROF_CNT)
 		s_prof.decayNdx = 0;
 	uint32_t freeNdx = PROF_CNT;
+	// >>> traverse to search existing same PC sample
 	for (i=0, pItem = s_prof.items; i<PROF_CNT; i++, pItem++) {
 		if (pItem->baseAddr == (pc & PROF_MASK)) {
 			_ProfOnHit(pItem, pc);
 			break;
 		} else if (freeNdx == PROF_CNT && pItem->hitCnt == 0){
+			// record empty item, in case need to use for new PC sample
 			freeNdx = i;
 		}
 	}
@@ -75,18 +82,20 @@ void SysTick_C_Handler(ExceptionRegisters_t *regs) {
 }
 
 #ifdef __CC_ARM
+
 __asm void SysTick_Handler(void) {
 	IMPORT	SysTick_C_Handler
 	PRESERVE8
 	tst lr, #4 
-    ite eq                 // Tell the assembler that the nest 2 instructions are if-then-else	
-	mrseq r0, msp		   // Make R0 point to main stack pointer
-	mrsne r0, psp		   // Make R0 point to process stack pointer
+    ite eq 
+	mrseq r0, msp
+	mrsne r0, psp
 	push   {lr}
-	bl SysTick_C_Handler  // Off to C land
+	bl SysTick_C_Handler
 	pop    {lr}
 	bx	   lr
 }
+
 #else
 __attribute__((naked))
 void SysTick_Handler(void) {
